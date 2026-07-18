d6d7430d62961d302e56dc75286819b3944f98ac
Rico Sonntag <mail@ricosonntag.de>
2026-07-18 22:35:59 +0200

    GH-26: Sort imports after merging main

--- tests/XmlEncoderTest.php
+++ tests/XmlEncoderTest.php
@@ -11,6 +11,9 @@ declare(strict_types=1);
 
 namespace MagicSunday\Test;
 
+use DOMDocument;
+use DOMElement;
+use DOMException;
 use MagicSunday\Test\Fixture\Author;
 use MagicSunday\Test\Fixture\BodyHost;
 use MagicSunday\Test\Fixture\Book;
@@ -24,6 +27,7 @@ use MagicSunday\Test\Fixture\NativeWithForeignDocblock;
 use MagicSunday\Test\Fixture\Person;
 use MagicSunday\Test\Fixture\PlainArrayHost;
 use MagicSunday\Test\Fixture\Price;
+use MagicSunday\Test\Fixture\UninitializedHost;
 use MagicSunday\Test\Fixture\UnionObjectHost;
 use MagicSunday\Test\Fixture\UnionProperty;
 use MagicSunday\Test\Fixture\UnmarkedNested;
@@ -32,17 +36,13 @@ use MagicSunday\XmlMapper\Annotation\XmlAttribute;
 use MagicSunday\XmlMapper\Annotation\XmlCDataSection;
 use MagicSunday\XmlMapper\Annotation\XmlNodeValue;
 use MagicSunday\XmlMapper\Converter\CamelCasePropertyNameConverter;
+use MagicSunday\XmlMapper\Converter\PropertyNameConverterInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\UsesClass;
 use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
-use DOMDocument;
-use DOMElement;
-use DOMException;
-use MagicSunday\Test\Fixture\UninitializedHost;
-use MagicSunday\XmlMapper\Converter\PropertyNameConverterInterface;
 
 /**
  * Behavioural characterization tests pinning the XML output produced by the encoder.
